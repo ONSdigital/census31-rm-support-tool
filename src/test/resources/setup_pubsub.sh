b137b902801d826aa6b3619d142d889c4ce8e9fd
Ryan Grundy <ryan.grundy@ons.gov.uk>
2026-09-07 15:00:21 +0100

    CN-185 Update refusal topic/subscription

--- src/test/resources/setup_pubsub.sh
+++ src/test/resources/setup_pubsub.sh
@@ -3,8 +3,8 @@
 # Wait for pubsub-emulator to come up
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' '"$PUBSUB_SETUP_HOST"')" != "200" ]]; do sleep 1; done'
 
-curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_refusal
-curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_refusal_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_refusal"}'
+curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_refusal-received
+curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_refusal-received_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_refusal-received"}'
 
 curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_invalid-case
 curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_invalid-case_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_invalid-case"}'
