33be34ef96c2e7eca8a77b09f0bffedf09e56bb1
Ryan Grundy <ryan.grundy@ons.gov.uk>
2026-09-10 11:47:44 +0100

    Updated pubsub topics/subscriptions

--- src/test/resources/setup_pubsub.sh
+++ src/test/resources/setup_pubsub.sh
@@ -6,8 +6,8 @@ bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' '"$PUBSUB_SETUP_HO
 curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_refusal
 curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_refusal_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_refusal"}'
 
-curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_invalid-case
-curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_invalid-case_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_invalid-case"}'
+curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_address-not-valid
+curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_address-not-valid_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_address-not-valid"}'
 
 curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/topics/event_fulfilment-request
 curl -X PUT http://"$PUBSUB_SETUP_HOST"/v1/projects/our-project/subscriptions/event_fulfilment-request_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/our-project/topics/event_fulfilment-request"}'
